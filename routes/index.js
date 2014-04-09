5467ea925f6edbcc2211856be29be2d60cd36266
Chris Makler <chris.makler@gmail.com>
2014-04-08 22:52:20 -0700

    add navigation

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,11 @@
 exports.index = function(req, res){
   res.render('index', { title: 'econgraphs.org' });
 };
+
+exports.demand = function(req, res){
+  res.render('demand', { title: 'Individual and Market Demand' });
+};
+
+exports.about = function(req, res){
+  res.render('about', { title: 'About EconGraphs' });
+};
